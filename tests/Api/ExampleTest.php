f9252fd36854a93e17af11d580278704017e305a
Wu Mobius <energy908@gmail.com>
2021-08-21 13:30:19 +0800

    chore: 修正 phpunit.xml > 新增 api 測試、 acceptance 測試資料夾
    issue: #22

---
+++ tests/Api/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
