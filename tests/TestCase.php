bc9e37efc17e58b710f5c6c6aac0193fde0e536f
Wu Mobius <energy908@gmail.com>
2021-08-30 21:23:47 +0800

    feat: 基礎 TestCase 補上 use Specify
    issue: #39

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,11 @@
 
 namespace Tests;
 
+use Codeception\Specify;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use Specify;
 }
